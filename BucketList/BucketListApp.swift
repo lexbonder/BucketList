ab1922932192d407a637a3182e590a41eed00831
Alex Bonder <lexbonder@gmail.com>
2023-08-25 08:56:13 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Alex Bonder on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
