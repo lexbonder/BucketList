6364f19caae2d14bff78ffd5320718a24d638079
Alex Bonder <lexbonder@gmail.com>
2023-08-25 09:26:42 -0400

    Hacking with Swift - 100 Days of SwiftUI - Comparable Protocol

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,15 +7,27 @@
 
 import SwiftUI
 
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+    
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+}
+
 struct ContentView: View {
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister")
+    ].sorted()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(users) { user in
+            Text("\(user.firstName) \(user.lastName)")
         }
-        .padding()
     }
 }
 
