fcba3f5bd3ff9448a80594f62569b10cd4285cd0
Alex Bonder <lexbonder@gmail.com>
2023-08-26 08:52:09 -0400

    Hacking with Swift - 100 Days of SwiftUI - Day 69 - MapKit

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,17 +5,48 @@
 //  Created by Alex Bonder on 8/25/23.
 //
 
+import MapKit
 import SwiftUI
 
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        VStack {
+                            Circle()
+                                .stroke(.red, lineWidth: 3)
+                                .frame(width: 44, height: 44)
+                                .onTapGesture { // onTapGesture interrupts navigation link
+                                    print("Tapped on \(location.name)")
+                                }
+                            Text(location.name)
+                                .padding(5)
+                                .background(.ultraThinMaterial)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
