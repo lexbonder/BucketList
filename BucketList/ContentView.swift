c2569b0adab0a5b43f168447beeecdb95265c0d5
Alex Bonder <lexbonder@gmail.com>
2023-08-26 09:21:11 -0400

    Hacking with Swift - 100 Days of SwiftUI - Day 69 - Biometrics

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,17 +5,42 @@
 //  Created by Alex Bonder on 8/25/23.
 //
 
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isUnlocked = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
         }
-        .padding()
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError? // variable - not constant!!
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // if it can do it, this will run.
+            let reason = "We need use your touchId to unluck your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    // authenticated successfully
+                    isUnlocked = true
+                } else {
+                    // failed for some reason
+                }
+            }
+        } else {
+            // no biomentrics saved on device
+        }
     }
 }
 
